0af23f2007eae069a8c594eba6bb937b1622aa21
Jokiv <jokiv@ya.ru>
2020-11-08 13:36:13 +0300

    Updated processing of meta.

--- ghidra_scripts/TransferEfiFilesToProject.java
+++ ghidra_scripts/TransferEfiFilesToProject.java
@@ -3,79 +3,63 @@
 //@UEFI
 
 import ghidra.app.util.headless.HeadlessScript;
+import ghidra.program.model.data.DataTypeManager;
+import ghidra.program.model.mem.MemoryAccessException;
 import ghidra.program.model.mem.MemoryBlock;
+import ghidra.util.Msg;
+import org.json.JSONObject;
 
 public class TransferEfiFilesToProject extends HeadlessScript {
 
 	@Override
 	protected void run() throws Exception {
 		String folderName = null;
-
+		this.setHeadlessImportDirectory(null);
+		String[] args = getScriptArgs();
+		if (args.length < 1)
+			return;
 		if (this.isRunningHeadless()) {
-			MemoryBlock childSmiBlock = this.getMemoryBlock("childSmi");
-			MemoryBlock regInterBlock = this.getMemoryBlock("regInterrupt");
-			this.setHeadlessImportDirectory(null);
-			String[] args = getScriptArgs();
-			if (args.length < 1)
-				return;
-			setHeadlessContinuationOption(HeadlessContinuationOption.CONTINUE);
-			
-			if (childSmiBlock != null) {
-				folderName = "ChildInterrupts";
-				print("ChildInterrupt found");
-			}
-			else {
-				print("No ChildInterrupt in module!");
-			}
-			if (regInterBlock != null) {
-				byte[] raw = new byte[(int) regInterBlock.getSize()];
-				regInterBlock.getBytes(regInterBlock.getStart(), raw);
-				String str = new String(raw);
-				str = str.substring(1, str.length() - 1);
-
-				String[] regInter = str.split("[, =]");
-				for (int i = 0; i < regInter.length; i++) {
-					switch (regInter[i]) {
-					case ("gpiHandler"):
-					case ("ichnHandler"):
-					case ("ioTrapHandler"):
-					case ("periodicTimerHandler"):
-					case ("pwrButtonHandler"):
-					case ("sxHandler"):
-					case ("usbHandler"):
-					case ("standbyButtonHandler"):
-					case ("pchTcoHandler"):
-					case ("pchPcieHandler"):
-					case ("pchAcpiHandler"):
-					case ("pchGpioUnlockHandler"):
-					case ("pchHandler"):
-					case ("pchEspiHandler"):
-					case ("acpiEnHandler"):
-					case ("acpiDisHandler"):
-						if (folderName == null || folderName.equalsIgnoreCase("UnknownInterrupts")) 
-							folderName = "HwInterrupts";
-						print("HwInterrupt found");
-						break;
-					case ("Unknown"):
-						if (folderName == null)
-							folderName = "UnknownInterrupts";
-						print("UnknownInterrupt found");
-						break;
-					case ("swSmiHandler"):
-						folderName = "SwInterrupts";
-						print("SwInterrupt found");
-						break;
-					}
-					if (folderName.compareTo("SwInterrupts") == 0)
-						break;
+			MemoryBlock metaBlock = this.getMemoryBlock("metaBlock");
+			if(metaBlock != null) {
+				byte[] raw = new byte[(int) metaBlock.getSize()];
+				try {
+					metaBlock.getBytes(metaBlock.getStart(), raw);
+				} catch (MemoryAccessException e) {
+					Msg.info(this, "Can't read metaBlock");
+					e.printStackTrace();
+					return;
 				}
-			} 
-			else {
-				print("No Sw/HwItterupts in module!");
-			}
+				String metaStr = new String(raw);
+				JSONObject meta = new JSONObject(metaStr);
+				JSONObject interrupts = meta.getJSONObject("interrupts");
+				JSONObject childSmi = interrupts.getJSONObject("child");
+				JSONObject swSmi = interrupts.getJSONObject("swSmi");
+				JSONObject hwSmi = interrupts.getJSONObject("hwSmi");
 
-			if (folderName != null) {
-				this.setHeadlessImportDirectory(args[0] + "/" + folderName);
+
+				if (!hwSmi.isEmpty()) {
+					folderName = "HwSmi";
+					print("HwSmi found");
+				}
+				else {
+					print("HwSmi is missing from the module!");
+				}
+				if (!childSmi.isEmpty()) {
+					folderName = "ChildInter";
+					print("ChildSmi found");
+				} else {
+					print("ChildInter is missing from the module!");
+				}
+				if (!swSmi.isEmpty()) {
+					folderName = "SwSmi";
+					print("SwSmi found");
+				} else {
+					print("SwSmi is missing from the module!");
+				}
+
+				if (folderName != null) {
+					this.setHeadlessImportDirectory(args[0] + "/" + folderName);
+				}
 			}
 		}
 	}
